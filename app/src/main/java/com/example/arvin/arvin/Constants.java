b2f7bbf844277db98c239b302935e6983fab4cc8
aravinth22 <aravinth9611991@gmail.com>
2017-09-18 14:42:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arvin/arvin/Constants.java
@@ -0,0 +1,11 @@
+package com.example.arvin.arvin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Constants  {
+    private static final String ROOT_URL ="http://192.168.8.100/android/v1/";
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
+
+
